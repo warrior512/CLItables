214c64fac65e0c5a25a1cf8df90f70210b0a58d4
warrior512 <nikolaysmirnov86@gmail.com>
2022-06-19 20:57:54 +0300

    add func createTablesFolder

--- CLItables.py
+++ CLItables.py
@@ -1,4 +1,19 @@
 from modules.interface import *
 
+
+def getSelfPath():
+    selfPath = os.path.abspath(__file__)
+    selfName = os.path.basename(selfPath)
+    return selfPath[:-len(selfName)]
+
+
+def createTablesFolder():
+    if not os.path.isdir(f'{getSelfPath()}tables'):
+        os.system(f'mkdir {getSelfPath()}tables')
+
+
 if __name__ == '__main__':
+    createTablesFolder()
     print_logo()
+    os.system(f'ls {getSelfPath()}tables') 
+    main_menu()
