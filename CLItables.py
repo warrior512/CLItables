1226b70df3d6e8304ca26136c7660a427e24420a
warrior512 <nikolaysmirnov86@gmail.com>
2022-06-19 22:17:48 +0300

    some code

--- CLItables.py
+++ CLItables.py
@@ -1,6 +1,9 @@
 from modules.interface import *
 
 
+tabFold = 'tables'
+
+
 def getSelfPath():
     selfPath = os.path.abspath(__file__)
     selfName = os.path.basename(selfPath)
@@ -8,12 +11,30 @@ def getSelfPath():
 
 
 def createTablesFolder():
-    if not os.path.isdir(f'{getSelfPath()}tables'):
-        os.system(f'mkdir {getSelfPath()}tables')
+    if not os.path.isdir(f'{getSelfPath()}{tabFold}'):
+        os.system(f'mkdir {getSelfPath()}{tabFold}')
+
+
+def createNewTable(newTableName):
+    if os.path.isfile(f'{getSelfPath()}{tabFold}/{newTableName}'):
+        return f'{newTableName} already exists'
+    elif os.system(f'touch {getSelfPath()}{tabFold}/{newTableName}') != 0:
+        return 'invalid filename'
+    
+
 
 
 if __name__ == '__main__':
     createTablesFolder()
-    print_logo()
-    os.system(f'ls {getSelfPath()}tables') 
-    main_menu()
+    while True:
+        print_logo()
+        os.system(f'ls {getSelfPath()}{tabFold}') 
+        main_menu()
+        action = input('> ').lower().strip()
+        if action == 'n':
+            tableName = input('new table name: ')
+            print(createNewTable(f'{tableName}.csv')
+            input('press enter to continue')
+            continue
+        else:
+            continue
